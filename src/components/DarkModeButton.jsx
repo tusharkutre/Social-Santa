2f0c6579b5e725873d5552f0009f71baf887268e
tushar kutre <tusharkutre21@gmail.com>
2025-05-07 21:09:32 +0530

    Add lucide-react dependency and update DarkModeButton component with icons; remove button border-radius in App.css

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -1,10 +1,13 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 import React from "react";
+
+
 
 const DarkModeButton = ({ toggleDarkMode, isDarkMode }) => {
   return (
     <>
-      <button onClick={toggleDarkMode} className={`toggle-btn font-semibold ${isDarkMode ? "bg-white text-black" : "bg-black text-white"}`}>
-        {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+      <button onClick={toggleDarkMode} className={`rounded-full toggle-btn font-semibold ${isDarkMode ? "bg-white text-black" : "bg-black text-white"}`}>
+        {isDarkMode ? <SunIcon className="h-6 w-6"/> : <MoonIcon className="h-6 w-6" />}
       </button>
     </>
   );
