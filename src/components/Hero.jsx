dadac0bf25438c7db116bc9146f2de87c4547d50
tushar kutre <tusharkutre21@gmail.com>
2025-05-07 22:04:06 +0530

    code updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,10 +24,12 @@ export default function Hero({ toggleDarkMode, isDarkMode }) {
         >
           <div className="flex  lg:flex-1">
             <a href="#" className="flex items-center flex-row-reverse gap-2">
-              <h2 className="text-2xl font-medium hidden md:block">Social Santa</h2>
+              <h2 className="text-2xl font-medium hidden md:block">
+                Social Santa
+              </h2>
               <svg
-                width="40"
-                height="40"
+                width="35"
+                height="35"
                 viewBox="0 0 49 40"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -39,7 +41,11 @@ export default function Hero({ toggleDarkMode, isDarkMode }) {
               </svg>
             </a>
           </div>
-          <div className="flex lg:hidden">
+          <div className={`flex lg:hidden rounded-full py-3 px-4 gap-3 ${isDarkMode ? "bg-white text-black" : "bg-slate-400/50 text-white"}`}>
+            <DarkModeButton
+              toggleDarkMode={toggleDarkMode}
+              isDarkMode={isDarkMode}
+            />
             <button
               type="button"
               onClick={() => setMobileMenuOpen(true)}
@@ -54,7 +60,11 @@ export default function Hero({ toggleDarkMode, isDarkMode }) {
               <a
                 key={item.name}
                 href={item.href}
-                className={`text-md font-semibold ${isDarkMode ? " text-white hover:bg-white hover:text-black px-2 py-2 rounded-2xl" : "text-black hover:bg-black hover:text-white px-2 py-2 rounded-2xl"} `}
+                className={`text-md font-semibold ${
+                  isDarkMode
+                    ? " text-white hover:bg-white hover:text-black px-2 py-2 rounded-2xl"
+                    : "text-black hover:bg-black hover:text-white px-2 py-2 rounded-2xl"
+                } `}
               >
                 {item.name}
               </a>
@@ -72,7 +82,7 @@ export default function Hero({ toggleDarkMode, isDarkMode }) {
         <Dialog
           open={mobileMenuOpen}
           onClose={setMobileMenuOpen}
-          className="lg:hidden"
+          className="lg:hidden block"
         >
           <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
@@ -80,18 +90,19 @@ export default function Hero({ toggleDarkMode, isDarkMode }) {
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <svg
-                width="40"
-                height="40"
-                viewBox="0 0 49 40"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M37.3947 40C43.8275 39.8689 49 34.6073 49 28.1389C49 24.9931 47.7512 21.9762 45.5282 19.7518L25.7895 0V12.2771C25.7895 14.3303 26.6046 16.2995 28.0556 17.7514L32.6795 22.3784L32.6921 22.3907L40.4452 30.149C40.697 30.4009 40.697 30.8094 40.4452 31.0613C40.1935 31.3133 39.7852 31.3133 39.5335 31.0613L36.861 28.3871H12.139L9.46655 31.0613C9.21476 31.3133 8.80654 31.3133 8.55476 31.0613C8.30297 30.8094 8.30297 30.4009 8.55475 30.149L16.3079 22.3907L16.3205 22.3784L20.9444 17.7514C22.3954 16.2995 23.2105 14.3303 23.2105 12.2771V0L3.47175 19.7518C1.24882 21.9762 0 24.9931 0 28.1389C0 34.6073 5.17252 39.8689 11.6053 40H37.3947Z"
-                  fill="#FF0A0A"
-                ></path>
-              </svg>
+                  width="40"
+                  height="40"
+                  viewBox="0 0 49 40"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M37.3947 40C43.8275 39.8689 49 34.6073 49 28.1389C49 24.9931 47.7512 21.9762 45.5282 19.7518L25.7895 0V12.2771C25.7895 14.3303 26.6046 16.2995 28.0556 17.7514L32.6795 22.3784L32.6921 22.3907L40.4452 30.149C40.697 30.4009 40.697 30.8094 40.4452 31.0613C40.1935 31.3133 39.7852 31.3133 39.5335 31.0613L36.861 28.3871H12.139L9.46655 31.0613C9.21476 31.3133 8.80654 31.3133 8.55476 31.0613C8.30297 30.8094 8.30297 30.4009 8.55475 30.149L16.3079 22.3907L16.3205 22.3784L20.9444 17.7514C22.3954 16.2995 23.2105 14.3303 23.2105 12.2771V0L3.47175 19.7518C1.24882 21.9762 0 24.9931 0 28.1389C0 34.6073 5.17252 39.8689 11.6053 40H37.3947Z"
+                    fill="#FF0A0A"
+                  ></path>
+                </svg>
               </a>
+
               <button
                 type="button"
                 onClick={() => setMobileMenuOpen(false)}
@@ -115,12 +126,7 @@ export default function Hero({ toggleDarkMode, isDarkMode }) {
                   ))}
                 </div>
 
-                <div className="">
-                  <DarkModeButton
-                    toggleDarkMode={toggleDarkMode}
-                    isDarkMode={isDarkMode}
-                  />
-                </div>
+                <div className=""></div>
               </div>
             </div>
           </DialogPanel>
@@ -155,7 +161,8 @@ export default function Hero({ toggleDarkMode, isDarkMode }) {
               Data to enrich your online business
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-            Our platform ensures swift, secure, and seamless delivery, empowering businesses to reach customers with ease.
+              Our platform ensures swift, secure, and seamless delivery,
+              empowering businesses to reach customers with ease.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
